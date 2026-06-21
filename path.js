b4ce0e1b77387b25dc20c2871cc40410706a9744
Puneet Kumar <myselfpunitkumar@gmail.com>
2026-06-21 16:50:35 +0530

    path_globalconstant

---
+++ path.js
@@ -0,0 +1,11 @@
+const { log } = require('console')
+const path = require('path')
+const file = 'text/peter.txt'
+// console.log(path.extname(file))
+// console.log(path.dirname(file))
+// console.log(path.basename(file)) 
+// log(path.resolve("text","peter.txt"))
+
+// log(__dirname)
+// log(__filename)
+
