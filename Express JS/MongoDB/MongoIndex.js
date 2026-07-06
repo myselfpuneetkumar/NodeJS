72816f127c571abc52efe5d2971f8c12d5225324
Puneet Kumar <myselfpunitkumar@gmail.com>
2026-07-06 19:52:25 +0530

    Mongodb Data on UI

--- Express JS/MongoDB/MongoIndex.js
+++ Express JS/MongoDB/MongoIndex.js
@@ -5,17 +5,20 @@ import {MongoClient} from 'mongodb'
 const dbName = "School"
 const url = "mongodb://localhost:27017/new"
 const client = new MongoClient(url);
-async function dbConnection(){
+
+
+const app = express();
+app.set('view engine','ejs')
+
+app.get('/',async(req,resp)=>{
     await client.connect();
     const db = client.db(dbName);
     const collection = db.collection('classes')
     const result = await collection.find().toArray()
     console.log(result);
-    
-}
-
-dbConnection();
-
-const app = express();
+    resp.render('students',{result})
+})
 app.listen(3000)
+
+
     
